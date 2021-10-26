9ad07917399df2aa33a0d55147d2d9a1d00c8b72
J Ka <gnemo.ka@gmail.com>
2021-10-26 16:28:22 +0200

    correction erreur de frappe package.json

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 
-const dist_dir = "/dist/imdb";
+const APP_PORT = process.env.PORT || 8080;
+const DIST_DIR = "/dist/imdb";
 
 const app = express();
-app.use(express.static(path.join(__dirname, dist_dir)));
-app.get("/*", (req, res) => res.sendFile("index.html", { root: dist_dir }));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(".", DIST_DIR)));
+app.get("/*", (req, res) => res.sendFile("index.html", { root: DIST_DIR }));
+app.listen(APP_PORT);
+console.log(`Running on port ${APP_PORT}`);
