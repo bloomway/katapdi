10b7c8d0c9a75a4040b849580b562b4621580792
J Ka <gnemo.ka@gmail.com>
2021-10-31 11:52:10 +0100

    test(MovieItemComponent): Teste le composant MovieItem

--- src/app/movie/infra/components/movie-item/movie-item.component.spec.ts
+++ src/app/movie/infra/components/movie-item/movie-item.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieItemComponent } from './movie-item.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {defaultMovie, Movie} from '../../../domain/movie.model';
+import {By} from '@angular/platform-browser';
 
 describe('MovieItemComponent', () => {
   let component: MovieItemComponent;
@@ -29,6 +30,23 @@ describe('MovieItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contains two images', () => {
+      const imgs = fixture.debugElement.queryAll(By.css('img'));
+      expect(imgs.length).toEqual(2);
+  });
+
+  it('should display icon pencil as a first image', () => {
+    const imgs = fixture.debugElement.queryAll(By.css('img'));
+    const pencil: HTMLImageElement = imgs[0].nativeElement;
+    expect(pencil.src).toContain('assets/images/icon_pencil.svg');
+  });
+
+  it('should display icon poster as a second image', () => {
+    const imgs = fixture.debugElement.queryAll(By.css('img'));
+    const poster: HTMLImageElement = imgs[1].nativeElement;
+    expect(poster.src).toContain('assets/images/icon_poster.svg');
   });
 
 });
